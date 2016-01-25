47725c12ac8203db36a08c3006445799400ed869
Ross Anderson <ross.anderson@ualberta.ca>
2016-01-25 11:00:47 -0700

    version: open(), exec() version.py in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from distutils.core import setup
 
-import version
-
 with open("README.md", "r") as fp:
     long_description = fp.read()
 
+version = dict()
+with open('version.py', 'r') as fp:
+    exec(fp.read(), version)
+
 setup(name="catan",
-      version=version.__version__,
+      version=version['__version__'],
       author="Ross Anderson",
       author_email="ross.anderson@ualberta.ca",
       url="https://github.com/rosshamish/catan-py/",
-      download_url = 'https://github.com/rosshamish/catan-py/tarball/' + version.__version__,
+      download_url = 'https://github.com/rosshamish/catan-py/tarball/' + version['__version__'],
       description="models for representing and manipulating a game of catan",
       long_description=long_description,
       keywords=[],
